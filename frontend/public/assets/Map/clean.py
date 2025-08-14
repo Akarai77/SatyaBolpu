e8e57099eb24cf0c6328845144cca908e6b58ed1
unknown <akarshrai05@gmail.com>
2025-08-14 23:25:31 +0530

    refactor: consisten geojsondata for all districts:

---
+++ frontend/public/assets/Map/clean.py
@@ -0,0 +1,18 @@
+import json
+
+districts = ['Dakshina Kannada','Udupi','Kasaragod']
+
+for district in districts:
+    with open(f'{district.lower().replace(' ','_')}.geojson', 'r+', encoding='utf-8') as geojson:
+        data = json.load(geojson)
+        for feature in data['features']:
+            feature['properties'] = {
+                'STATE': 'Karnataka',
+                'DISTRICT': district,
+                'TALUK': '',
+                'VILLAGE': feature['properties']['Village_Name']
+            }
+        
+        geojson.seek(0)
+        json.dump(data, geojson, indent=2, ensure_ascii=False)
+        geojson.truncate()
